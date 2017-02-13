3b85fce12a6d15de780b2594fed1739117f16e73
MICKAEL GERMEMONT <mickaelgermemont@mickaels-macbook-pro.local>
2017-02-12 22:40:45 -0500

    Initial Commit

---
+++ RunningCoins/RunningCoins.h
@@ -0,0 +1,19 @@
+//
+//  RunningCoins.h
+//  RunningCoins
+//
+//  Created by MICKAEL GERMEMONT on 2/12/17.
+//  Copyright © 2017 MICKAEL GERMEMONT. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RunningCoins.
+FOUNDATION_EXPORT double RunningCoinsVersionNumber;
+
+//! Project version string for RunningCoins.
+FOUNDATION_EXPORT const unsigned char RunningCoinsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RunningCoins/PublicHeader.h>
+
+
